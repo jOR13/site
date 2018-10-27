67cda8689c2b2c8d44af7797131a2813fd87549a
Jesús Ochoa Rabelo <ochoarj13@hotmail.com>
2018-10-26 20:42:06 -0600

    Update GamePlay.js

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -1,58 +1,30 @@
+float n = 0;
+float c = 3;
 
-GamePlayManager = {
-    init: function() {
-        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        game.scale.pageAlignHorizontally=true;
-        game.scale.pageAlignVertically=true;
-        
-        this.flagFirstMouseDown =false;
-    },
-    preload: function() {
-        game.load.image('background', 'assets/images/background.png');
-        game.load.spritesheet('horse', 'assets/images/horse.png',84,156,2);
-        game.load.spritesheet('diamonds', 'assets/images/diamonds.png',81,84,4);
-    },
-    create: function() {
-        game.add.sprite(0,0,'background');
-        this.horse = game.add.sprite(0,0,'horse');
-        this.horse.frame=0;
-        this.horse.x = game.width*.5;
-        this.horse.y = game.height*.5;
-        this.horse.anchor.setTo(0.5);
-        
-        game.input.onDown.add(this.onTap, this);
-    },
-    
-    onTap: function(){
-        
-    this.flagFirstMouseDown = true;
-    
-    },
-    
-    update: function() {
-       if(this.flagFirstMouseDown){
-        var pointerX = game.input.x;
-        var pointerY = game.input.y;
-        
-        var distX = pointerX - this.horse.x;
-        var distY = pointerY - this.horse.y;
-        
-        if(distX>0){
-            this.horse.scale.setTo(1,1);
-        }else{
-            
-            this.horse.scale.setTo(-1,1);
-        }
-        
-        
-        this.horse.x += distX * .04;
-        this.horse.y += distY * .04;
-        this.horse.y += distY * .04;
-       }
-    }
+ArrayList<PVector> points = new ArrayList<PVector>();
+
+float start = 0;
+
+void setup() {
+  size(400, 400);
+  colorMode(HSB, 360, 255, 255);
 }
 
-var game = new Phaser.Game(1136, 640, Phaser.CANVAS);
-    
-game.state.add("gameplay", GamePlayManager);
-game.state.start("gameplay");
+void draw() {
+  background(0);
+  translate(width / 2, height / 2);
+  rotate(n * 0.3);
+  for (int i = 0; i < n; i++) {
+    float a = i * radians(137.5);
+    float r = c * sqrt(i);
+    float x = r * cos(a);
+    float y = r * sin(a);
+    float hu = i+start;//sin(start + i * 0.5);
+    hu = i/3.0 % 360;
+    fill(hu, 255, 255);
+    noStroke();
+    ellipse(x, y, 4, 4);
+  }
+  n += 5;
+  start += 5;
+}
